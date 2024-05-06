299ecbb3a90391c289d1857a091bf1a6df1c1c36
root <root@vm2542356.firstbyte.club>
2024-05-06 22:16:25 +0300

    add code

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+/**
+ * 
+ */
+$csv = file('e.csv', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+$headers = str_getcsv(array_shift($csv), ','); 
+
+foreach ($csv as $line) {
+    $products[] = array_combine(
+        $headers,
+        str_getcsv($line, ','),
+    );
+}
+
+usort($products, function($a, $b){
+    return ($a['parent_id'] - $b['parent_id']);
+});
+
+$list = [];
+$listIndex = [];
+
+foreach ($products as $val) {
+    $elem = ['name' => $val['name']];
+    if ($val['parent_id'] == 0) {
+        $list[(string)$val['id']] = $elem;
+        $listIndex[$val['id']] = &$list[(string)$val['id']];
+    } 
+    else {
+        if (($list[$val['parent_id']]??0) != 0) {
+            $list[(string)$val['parent_id']]['children'][(string)$val['id']] = $elem;
+            $listIndex[$val['id']] = &$list[(string)$val['parent_id']]['children'][(string)$val['id']];
+        }
+        else {
+            foreach ($listIndex as $key => &$valIndex) {
+                if ($key == $val['parent_id']) {
+                    if (($valIndex['children']??0) == 0) {
+                        $valIndex['children'] = [];
+                    }
+
+                    $valIndex['children'][$val['id']] = $elem;
+                    $listIndex[$val['id']] = &$valIndex['children'][$val['id']];
+                }
+            }
+        }
+    }
+}
+
+echo json_encode(
+    [
+        'data' => $products,
+        'result' => $list
+    ]
+);
